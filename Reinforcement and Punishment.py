96a96526fc8ecd063103c6f72b36f76c83afb4d1
Brent Porter <eporter2@live.maryville.edu>
2018-07-08 12:42:21 -0600

    Initial commit

---
+++ Reinforcement and Punishment.py
@@ -0,0 +1,22 @@
+#multiple calculator
+def multiple(x,y):
+    x=x
+    y=y
+    
+    if y % x == 0:
+        print ( '{0} is a multiple of {1}!'.format(y,x) )
+    else:
+        print ('{0} is NOT a multiple of {1}!'.format(y,x) )
+
+def main():
+    x = int(input("Please give me a factor: "))
+    y = int(input("Please give me a number to check for multiples: "))
+    multiple(x,y)
+    
+main()
+    
+
+#Spam Statement repeater
+for sentance in range (100):
+    print('I will never spam my friends again.')
+    
